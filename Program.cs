564da7e651d550f905417b19195127f0692faf16
nadhiraaurell <nadhiraurelll@gmail.com>
2022-03-23 21:26:41 +0700

    commit pertama

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat object Taxi
+            Taxi taxi = new Taxi();
+
+            // pengetesan nilai properties
+            taxi.DriverName = "Jono";
+            taxi.Onduty = true;
+            taxi.NumPassenger = 10;
+
+            // pemanggilan methode Taxi Info, Pick Up Passenger, dan Drop Off Passenger
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxi
+{
+   public class Taxi
+    {
+        // properties 
+        public string DriverName { get; set; }
+        public bool Onduty { get; set; }
+        bool duty = true;
+        public int NumPassenger { get; set; }
+
+        // methode
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            Console.WriteLine("On Duty: {0}", duty ? "yes" : "No");
+            Console.WriteLine("Number of Passenger: {0}", NumPassenger);
+            Console.WriteLine();
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai menjemput penumpang", DriverName);
+        }
+    }
+}
